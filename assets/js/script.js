490f468703127e1b539e58fdcbccbefc4da53953
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-03-30 21:08:24 +0700

    Update TTPmart Page

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Slick
+// Slide
+$(document).ready(function () {
+  $('.inner-slide').slick({
+    infinite: true,
+    fade: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+});
+
+
+// End Slide
+// End Slick
