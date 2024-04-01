7b48765d00edf08225adb2492d7e95a866192cbc
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2024-04-01 15:32:34 +0700

    Update Home Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,20 @@ $(document).ready(function () {
   $('.inner-slide').slick({
     infinite: true,
     fade: true,
-    slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+  });
+
+  // Bắt sự kiện khi slide được chuyển
+  $('.inner-slide').on('afterChange', function(event, slick, currentSlide){
+    // Gọi WOW.init() trên slide hiện tại
+    new WOW({}).init();
   });
 });
-
-
 // End Slide
+
+// Brand
+$('#brand').slick();
+// End Brand
+
 // End Slick
